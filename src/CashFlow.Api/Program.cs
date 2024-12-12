440ee79827348c14cacc7ff0bb981791c094ab54
robsu17 <robson.costa134@gmail.com>
2024-12-12 07:32:59 -0300

    feat: tratando exceções

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//builder.Services.AddRouting((route) => route.LowercaseUrls());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,43 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if (titleIsEmpty) 
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero.");
+        }
+
+        var compareDate = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (compareDate > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException($"Invalid payment type {request.PaymentType}.");
+        }
+    }
+}
